653de5254924c53a7c5254a101d3fe686529084e
Andrei Duboc <andreibono@hotmail.com>
2019-11-21 18:31:47 -0400

    Project Created

---
+++ src/main/java/com/ssys/employeesreport/EmployeesreportApplication.java
@@ -0,0 +1,13 @@
+package com.ssys.employeesreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesreportApplication.class, args);
+	}
+
+}
